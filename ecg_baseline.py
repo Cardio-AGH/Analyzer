7c4d0f3338c602eb631147156fdf6fa7d81eb9e0
jkrzy <j.krzywdziak@gmail.com>
2022-11-16 19:30:44 +0100

    changed logic for script, wrote TODO

--- ecg_baseline.py
+++ ecg_baseline.py
@@ -1,17 +1,27 @@
 import numpy as np
 import scipy.signal as signal
+# import for lms filter
 import padasip as pa
 
 
-def moving_average(data, window_size):
-    filtered = np.convolve(data, np.ones(window_size) / window_size, mode='valid')
-    return filtered
+class BaselineECG:
+    def __init__(self, data, json_params):
+        self.data = data
+        self.fs = json_params['fs']
+        self.N = json_params['n_sig']
 
-def butterworth_filter(data, N, fs, Wn)
-    b_butter, a_butter = signal.butter(N, Wn=Wn, fs=fs, btype="high")
-    return signal.filtfilt(b_butter, a_butter, data)
+    def moving_average(self, window_size):
+        filtered = np.convolve(self.data, np.ones(window_size) / window_size, mode='valid')
+        return filtered
 
-def sav_goal_filter(data, window_length, polyorder, mode):
-    return signal.savgol_filter(data, window_length, polyorder, mode)
+    def butterworth_filter(self, Wn):
+        b_butter, a_butter = signal.butter(self.N, Wn, self.fs, btype="high")
+        return signal.filtfilt(b_butter, a_butter, self.data)
 
-def lms_filter()
+    def sav_goal_filter(self, window_length):
+        return signal.savgol_filter(self.data, window_length, 3)
+
+    def lms_filter(self):
+        pass
+
+# TODO: finish lms_filter, ask about missing parameter values, do main funct.
